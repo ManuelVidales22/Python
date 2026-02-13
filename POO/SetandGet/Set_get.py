a86e42cccd56b9e1ec4bdb45f0fd94c3f9b30233
ManuelVidales22 <manuelvidales33@gmail.com>
2026-02-13 15:25:55 -0500

    Cambios

--- POO/SetandGet/Set_get.py
+++ POO/SetandGet/Set_get.py
@@ -2,16 +2,25 @@ class Persona:
     def __init__(self, nombre, edad):
         self._nombre = nombre
         self._edad = edad
-
+        
+    "El Get y Set son metodos para obtener y modificar atributos, ya sean privados o publicos, se pueden usar para validar datos antes de asignarlos."
     def get_nombre(self):
         return self._nombre
     
     def set_nombre(self, nuevo_nombre):
         self._nombre = nuevo_nombre
+        
+    def get_edad(self):
+        return self._edad
+    
+    def set_edad(self, nueva_edad):
+        self._edad = nueva_edad
+        
+    
 
 
 manuel = Persona("Manuel", 30)
 
 manuel.set_nombre("Manuel Gomez")
-nombre = manuel.get_nombre()
-print(nombre)
+manuel.set_edad(31)
+print(f"el nombre es {manuel.get_nombre()} y la edad es {manuel.get_edad()}")  
